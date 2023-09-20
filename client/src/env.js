087c17429cbf20ae618a8d7b1d48dc18e0bb4178
Jason Millhorn <jdmillhorn@gmail.com>
2023-09-19 19:02:08 -0600

    env

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'htlozby8o1yf6we2.us.auth0.com'
+export const domain = 'dev-htlozby8o1yf6we2.us.auth0.com'
 export const clientId = 'CXj7zZ9a2ctjU63QU6QDTnlLa08mrv34'
 export const audience = 'https://codeworksjd.com'
